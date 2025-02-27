6a330192755ed6c3bfc2546b856e45ec8e1adcdb
Khushi <imkhushij0722@gmail.com>
2025-02-27 19:45:22 +0530

    project struture ready evnv setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Khushi",
+    author_email="imkhushij0722@gmail.com",
+    packages=find_packages()
+)
